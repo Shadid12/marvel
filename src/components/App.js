5043a6819367ca88f03795532fdbbaaa8dd45a79
shadid12 <shadidhaque2014@gmail.com>
2017-05-11 15:36:48 -0400

    initial barebone stuff

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { Component } from 'react'
+import { Nav } from './Nav'
+
+export class App extends Component {
+
+	render(){
+		return(
+			<div>
+				<Nav />
+			</div>
+		)
+	}
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import { Component } from 'react'
+
+export class Nav extends Component {
+
+	render(){
+		return(
+			<div>
+			<h1>I am a NavBar</h1>
+			</div>
+		)
+	}
+}
