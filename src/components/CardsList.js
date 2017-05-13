ae4bdfd7ff1da3443fb9da9dd7350eb75c86e88f
shadid12 <shadidhaque2014@gmail.com>
2017-05-13 02:26:44 -0400

    done basics

--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -5,7 +5,7 @@ export const CardsList = (props) => {
 	return (
 		<MuiThemeProvider>
 	      <div className="row">
-	        	{props.cards.map( card => <Cards key={card.id} {...card}/>)}
+	        {props.cards.map( card => <Cards key={card.id} {...card}/>)}
 	      </div>
 		</MuiThemeProvider>	
 	)
@@ -17,19 +17,25 @@ export const Cards = (props) => {
 
 	
 	return(
-	<div className="col s12 m7">
-	  <div className="card">
-	    <div className="card-image">
-	      <img src={thumb}/>
-	      <span className="card-title">{props.name}</span>
-	    </div>
-	    <div className="card-content">
-	      <p>{props.description}</p>
-	    </div>
-	    <div className="card-action">
-	      <RaisedButton label="Read More" secondary={true} />
-	    </div>
-	  </div>
-	</div>
+		<div>
+			<div className="col s12 m5">
+				<div className="card">
+					<div className="card-image">
+						<img src={thumb}/>
+						<span className="card-title">{props.name}</span>
+					</div>
+					<div className="card-action">
+						<RaisedButton label="Details" 
+									  secondary={true} 
+									  href={props.urls[0].url} />
+					</div>
+				</div>
+			</div>
+			<br />
+			<div className="col s7">
+				{props.description}
+			</div>
+		</div>
+
 	)
 }
